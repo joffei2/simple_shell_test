ffc6556ebfca6dc47ecea01ef927d1c227d8b1d2
joffei2 <joffei112@gmail.com>
2024-02-27 16:16:46 +0000

    ...

--- main.h
+++ main.h
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 void arg(char **argv);
 char *path(char *command);
--- shellPath.c
+++ shellPath.c
@@ -2,7 +2,52 @@
 
 char *getPath(char *command)
 {
-	char *path;
-	char *copy_path;
+	char *path; *filePath
+		char *copy_path;
+	int cmdLenght;
+	char *pathToken;
+	int directoryLenght;
+	struct stat buffer;
 
 	path = getenv("PATH");
+
+	if(path)
+	{
+		copy_path = strdup(path);
+
+		cmdLenght = strlen(command);
+
+		pathToken = strtok(copy_path, ":");
+
+		while(pathToken != NULL)
+		{
+			directoryLenght = strlen(pathToken);
+
+			filePath = malloc(commandLenght + directoryLenght + 2);
+
+			strcpy(filePath, pathToken);
+			strcat(filePath, "/");
+			strcat(filePath, command);
+			strcat(filePath, "\0");
+
+			if (stat(filePath, &buffer) == 0)
+			{
+				free(copyPath);
+				return (filePath);
+			}
+			else
+			{
+				free(filePath);
+				pathToken = strtok(Null, ":");
+			}
+		}
+		free(copy_path):
+
+			if (stat(command, &buffer) == 0)
+			{
+				return (command);
+			}
+		return (NULL);
+	}
+	return (NULL);
+}
